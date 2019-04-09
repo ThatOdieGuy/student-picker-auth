cf7d912ef17c67658fd693b6bc46086aeafdc785
Mike Odom <5105729+ThatOdieGuy@users.noreply.github.com>
2019-04-09 10:00:05 -0700

    Adding logout support

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import { withRouter } from "react-router";
+
 import "./NavBar.css";
 
 // TODO: Highlight these links
 
-export default function NavBar() {
+function NavBar(props) {
+  function logoutCheck() {
+    if (window.confirm("Logout of this account?")) {
+      axios.post("/api/user/logout").then(response => {
+        props.history.push("/");
+      })
+    }
+  }
+
   return (
     <div>
-      <NavLink to="/RegistrationPage" className="navlink" activeClassName="selected">Registration Page</NavLink>{" "}
-      <NavLink to="/LoginPage" className="navlink" activeClassName="selected">Login Page</NavLink>{" "}
+      <NavLink to="/RegistrationPage" className="navlink" activeClassName="selected">Registration Page</NavLink>
+      <NavLink to="/LoginPage" className="navlink" activeClassName="selected">Login Page</NavLink>
       <NavLink to="/UserHomePage" className="navlink" activeClassName="selected">User Home Page</NavLink>
+      <a href="#" className="navlink" onClick={logoutCheck}>Logout</a>
     </div>
   )
 }
+
+// By wrapping this component in `withRouter`, we get access to props.history
+//  https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/withRouter.md
+export default withRouter(NavBar);
